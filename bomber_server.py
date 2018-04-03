020ee368d13ea00a50b2436cee31e8ba04ba038e
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2018-04-03 14:12:20 +0200

    add client & server ref in evm

--- bomber_server.py
+++ bomber_server.py
@@ -36,14 +36,15 @@ model = Model()
 model.load_map(map_file)
 for _ in range(10): model.add_fruit()
 evm = EventManagerServer(model)
-network = NetworkServerController(model, evm, port)
+server = NetworkServerController(model, evm, port)
+evm.setNetworkServerController(server)
 # view = GraphicView(model, "server")
 
 # main loop
 while True:
     # make sure game doesn't run at more than FPS frames per second
     dt = clock.tick(FPS)
-    network.tick(dt)
+    server.tick(dt)
     model.tick(dt)
     # view.tick(dt)
 
